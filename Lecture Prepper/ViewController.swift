7c5bef5317fb1d4d202c265a05441a13ca95616c
Arthur <Arthur.Lai89@gmail.com>
2020-05-06 05:22:05 -0400

    Initial Commit

---
+++ Lecture Prepper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lecture Prepper
+//
+//  Created by Arthur on 5/6/20.
+//  Copyright © 2020 Arthur Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
