736ecaebbacf30d7a6b5cb097d20bdb2fef5930c
Arthur <Arthur.Lai89@gmail.com>
2020-09-11 17:29:45 -0400

    commit

---
+++ Lecture Prepper/DeleteAllViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteAllViewController.swift
+//  Lecture Prepper
+//
+//  Created by Arthur on 5/15/20.
+//  Copyright © 2020 Arthur Lai. All rights reserved.
+//
+
+import UIKit
+
+class DeleteAllViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lecture Prepper/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Lecture Prepper
+//
+//  Created by Arthur on 5/16/20.
+//  Copyright © 2020 Arthur Lai. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
